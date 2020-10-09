074c749764417043af2ae2bfe57aba96b52fef08
Sam <sub294c@gmail.com>
2020-10-08 20:13:15 -0400

    first commit

---
+++ build/precache-manifest.8f75f2bb4b3a27c3c86f352d2bbeec42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a85baf2194743d820d3c2fa48f57fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10852e51961ffa37fced",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2343ff554e87a954cfb8",
+    "url": "/static/js/2.56b5ee51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56b5ee51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10852e51961ffa37fced",
+    "url": "/static/js/main.aacc676f.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
